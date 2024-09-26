782059f685b91df6e0e5687e52e70d55ad110336
joo-chang <jooc0311@gmail.com>
2024-09-26 15:33:36 +0900

    init commit

---
+++ application/concert-service/src/main/java/com/fortickets/concertservice/ConcertServiceApplication.java
@@ -0,0 +1,12 @@
+package com.fortickets.concertservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcertServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcertServiceApplication.class, args);
+    }
+}
