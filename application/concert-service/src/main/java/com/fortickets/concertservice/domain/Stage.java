25808650fa6f7586ecd9bb5c210585e010a7e31f
uzjaee <kyj960802@gmail.com>
2024-10-02 08:54:27 +0900

    [#18] feat(concert) : concert , stage, Schedule 엔티티 추가및 연관관계 설정

---
+++ application/concert-service/src/main/java/com/fortickets/concertservice/domain/Stage.java
@@ -0,0 +1,34 @@
+package com.fortickets.concertservice.domain;
+
+import com.fortickets.jpa.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "stage")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Stage extends BaseEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "stage_id")
+  private Long stageId;
+
+  @Column(name = "name", length = 100, nullable = false)
+  private String name;
+
+  @Column(name = "location", length = 255, nullable = false)
+  private String location;
+
+  @Column(name = "row", nullable = false)
+  private Integer row;
+
+  @Column(name = "col", nullable = false)
+  private Integer col;
+
+}
