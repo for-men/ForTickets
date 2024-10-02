24ca2c14dcfa2a07869e265c707ee9aba5a6b2c7
uzjaee <kyj960802@gmail.com>
2024-10-02 21:49:40 +0900

    #18 chore(concert) : concertrepo @repository 삭제

--- application/concert-service/src/main/java/com/fortickets/concertservice/domain/repository/ConcertRepository.java
+++ application/concert-service/src/main/java/com/fortickets/concertservice/domain/repository/ConcertRepository.java
@@ -4,7 +4,7 @@ import com.fortickets.concertservice.domain.entity.Concert;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+
 public interface ConcertRepository extends JpaRepository<Concert, Long> {
 
 }
