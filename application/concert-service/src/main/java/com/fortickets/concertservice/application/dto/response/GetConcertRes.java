c3212dab3c76ac19e44f807f522dddb9186f84bf
uzjaee <kyj960802@gmail.com>
2024-10-03 12:31:12 +0900

    #18 chore : GetConcertRes 내 날짜 타입 수정 As-is : Date To-be : LocalDate

--- application/concert-service/src/main/java/com/fortickets/concertservice/application/dto/response/GetConcertRes.java
+++ application/concert-service/src/main/java/com/fortickets/concertservice/application/dto/response/GetConcertRes.java
@@ -1,6 +1,6 @@
 package com.fortickets.concertservice.application.dto.response;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 public record GetConcertRes(
     Long concertId,
@@ -8,8 +8,8 @@ public record GetConcertRes(
     String concertName,
     String concertImage,
     int runtime,
-    Date startDate,
-    Date endDate,
+    LocalDate startDate,
+    LocalDate endDate,
     Long price
 ) {
 
