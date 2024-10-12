0ce8c83e2e003537a0c4dbd540e42601ea2659de
joo-chang <jooc0311@gmail.com>
2024-10-13 01:40:03 +0900

    #60 refactor(order) : 중복 좌석 예약 체크

--- application/order-service/src/main/java/com/fortickets/orderservice/application/service/BookingService.java
+++ application/order-service/src/main/java/com/fortickets/orderservice/application/service/BookingService.java
@@ -8,10 +8,10 @@ import com.fortickets.orderservice.application.client.UserClient;
 import com.fortickets.orderservice.application.dto.request.ConfirmBookingReq;
 import com.fortickets.orderservice.application.dto.request.CreateBookingReq;
 import com.fortickets.orderservice.application.dto.request.CreatePaymentReq;
-import com.fortickets.orderservice.application.dto.response.GetConcertDetailRes;
-import com.fortickets.orderservice.application.dto.response.GetConcertRes;
 import com.fortickets.orderservice.application.dto.response.CreateBookingRes;
 import com.fortickets.orderservice.application.dto.response.GetBookingRes;
+import com.fortickets.orderservice.application.dto.response.GetConcertDetailRes;
+import com.fortickets.orderservice.application.dto.response.GetConcertRes;
 import com.fortickets.orderservice.application.dto.response.GetScheduleDetailRes;
 import com.fortickets.orderservice.application.dto.response.GetUserRes;
 import com.fortickets.orderservice.domain.entity.Booking;
@@ -21,8 +21,10 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
@@ -32,7 +34,6 @@ import org.redisson.api.RedissonClient;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
-import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
@@ -42,6 +43,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 @Slf4j
 public class BookingService {
+
     private final BookingRepository bookingRepository;
     private final BookingMapper bookingMapper;
     private final UserClient userClient;
@@ -54,6 +56,13 @@ public class BookingService {
 
         log.info("createBookingReq : {}", createBookingReq);
 
+        // 중복 좌석 예약 확인
+        if (createBookingReq.seat().size() >= 2) {
+            Set<String> set = new HashSet<>();
+            if (createBookingReq.seat().stream().anyMatch(e -> !set.add(e))) {
+                throw new GlobalException(ErrorCase.DUPLICATE_SEAT);
+            }
+        }
         // 분산 락 이름 정의 (예약 스케줄 ID 기반)
         String lockKey = "bookingLock:" + createBookingReq.scheduleId();
         RLock lock = redissonClient.getLock(lockKey); // 락 객체 생성
@@ -71,9 +80,9 @@ public class BookingService {
             List<Booking> bookings = new ArrayList<>();
             createBookingReq.seat().forEach(seat -> {
                 bookingRepository.findByScheduleIdAndSeat(createBookingReq.scheduleId(), seat)
-                        .ifPresent(booking -> {
-                            throw new GlobalException(ErrorCase.ALREADY_BOOKED_SEAT);
-                        });
+                    .ifPresent(booking -> {
+                        throw new GlobalException(ErrorCase.ALREADY_BOOKED_SEAT);
+                    });
                 Booking booking = createBookingReq.toEntity(seat);
                 bookings.add(booking);
             });
@@ -93,7 +102,11 @@ public class BookingService {
 
     @Transactional
     public void confirmBooking(ConfirmBookingReq confirmBookingReq) {
-        List<Booking> bookingList = bookingRepository.findAllByIdInAndStatus(confirmBookingReq.bookingIds(), BookingStatus.PENDING);
+        // 예매 정보 조회
+        List<Booking> bookingList = bookingRepository.findAllByIdInAndStatusAndUserId(
+            confirmBookingReq.bookingIds(), BookingStatus.PENDING,
+            confirmBookingReq.userId());
+
         if (bookingList.isEmpty()) {
             throw new GlobalException(ErrorCase.BOOKING_NOT_FOUND);
         }
@@ -135,7 +148,6 @@ public class BookingService {
         Map<Long, GetConcertRes> concertMap = concertList.stream()
             .collect(Collectors.toMap(GetConcertRes::id, Function.identity()));
 
-
         // Booking의 concertId와 매칭되는 GetConcertRes를 찾아 매핑
         List<GetBookingRes> getBookingResList = bookingList.getContent().stream()
             .map(booking -> {
@@ -146,7 +158,8 @@ public class BookingService {
         return new PageImpl<>(getBookingResList, pageable, bookingList.getTotalElements());
     }
 
-    public Page<GetBookingRes> getBookingBySeller(Long userId, Long sellerId, String role, String nickname, String concertName, Pageable pageable) {
+    public Page<GetBookingRes> getBookingBySeller(Long userId, Long sellerId, String role, String nickname, String concertName,
+        Pageable pageable) {
         // 판매자와 요청자가 같은지 확인
         // TODO: @PreAuthorize 는 MANAGER 인데 X-Role은 ROLE_MANAGER?
         if (!role.equals("ROLE_MANAGER")) {
@@ -164,8 +177,8 @@ public class BookingService {
         }
         if (concertName != null) {
             // 공연명으로 공연 조회
-           concertList = concertClient.searchConcert(sellerId, concertName);
-        }else {
+            concertList = concertClient.searchConcert(sellerId, concertName);
+        } else {
             concertList = concertClient.getConcertBySeller(sellerId);
         }
 
@@ -218,7 +231,7 @@ public class BookingService {
             throw new GlobalException(ErrorCase.CANNOT_CANCEL_BOOKING);
         }
 
-         paymentService.cancelPayment(booking.getPayment().getId());
+        paymentService.cancelPayment(booking.getPayment().getId());
     }
 
     @Transactional
@@ -235,7 +248,7 @@ public class BookingService {
     public List<String> getSeatsByScheduleId(Long scheduleId) {
         BookingStatus pending = BookingStatus.PENDING;
         BookingStatus confirmed = BookingStatus.CONFIRMED;
-        return bookingRepository.findSeatByScheduleId(scheduleId,pending,confirmed);
+        return bookingRepository.findSeatByScheduleId(scheduleId, pending, confirmed);
     }
 
 }
