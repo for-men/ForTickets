372d2f9266210572d59b26d3cbcf01f610327db9
joo-chang <jooc0311@gmail.com>
2024-10-14 12:42:47 +0900

    #60 fix(seucurity) : 예제 코드 삭제

--- application/user-service/src/main/java/com/fortickets/userservice/application/security/SecurityConfig.java
+++ application/user-service/src/main/java/com/fortickets/userservice/application/security/SecurityConfig.java
@@ -58,19 +58,14 @@ public class SecurityConfig {
 
         // 요청 권한 설정
         http.authorizeHttpRequests((authorizeHttpRequests) ->
-                authorizeHttpRequests
-                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // resources 접근 허용 설정
-                        .requestMatchers("/").permitAll() // 메인 페이지 요청 허가
-                        .requestMatchers("/auth/**").permitAll() // '/api/v1/auth/'로 시작하는 요청 모두 접근 허가
-                        .requestMatchers("/actuator/prometheus").permitAll()
-                        .anyRequest().authenticated() // 그 외 모든 요청 인증처리
+            authorizeHttpRequests
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // resources 접근 허용 설정
+                .requestMatchers("/").permitAll() // 메인 페이지 요청 허가
+                .requestMatchers("/auth/**").permitAll() // '/api/v1/auth/'로 시작하는 요청 모두 접근 허가
+                .requestMatchers("/actuator/prometheus").permitAll()
+                .anyRequest().authenticated() // 그 외 모든 요청 인증처리
         );
 
-        if (url == "/user-service") {
-            http.addFilterBefore(jwtAuthorizationFilterForUserService(), UsernamePasswordAuthenticationFilter.class);
-        } else if (){
-            http.addFilterBefore(jwtAuthenticationFilterForCommon(), UsernamePasswordAuthenticationFilter.class);
-        }
         // 필터 관리
 //        http.addFilterBefore(jwtAuthenticationFilterForCommon(), UsernamePasswordAuthenticationFilter.class);
         http.addFilterBefore(jwtAuthorizationFilterForUserService(), UsernamePasswordAuthenticationFilter.class);
