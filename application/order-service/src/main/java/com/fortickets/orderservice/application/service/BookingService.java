913c830c12a3813d38d2eca0a639757f962397dc
sangwoo lee <lswoo0705@gmail.com>
2024-10-21 00:38:05 +0900

    #75 feat(booking) : 예매 취소 kafka 비동기 처리 수정

--- application/order-service/src/main/java/com/fortickets/orderservice/application/service/BookingService.java
+++ application/order-service/src/main/java/com/fortickets/orderservice/application/service/BookingService.java
@@ -265,35 +265,6 @@ public class BookingService {
     }
 
     // ================= 완성 코드 =================
-//    @Transactional
-//    public void cancelBooking(Long getUserId, String role, Long bookingId) {
-//
-//        // TODO : 이거 좀 애매함 확인 필요 -> 결제 취소
-//        Booking booking = bookingRepository.findById(bookingId)
-//            .orElseThrow(() -> new GlobalException(ErrorCase.BOOKING_NOT_FOUND));
-//
-//        if (!role.equals("ROLE_MANAGER")) {
-//            if (!getUserId.equals(booking.getUserId())) {
-//                throw new GlobalException(ErrorCase.NOT_AUTHORIZED);
-//            }
-//        }
-//        GetScheduleDetailRes scheduleRes = concertClient.getScheduleDetail(booking.getScheduleId());
-//
-//        // 공연이 끝났는지 체크 필요
-//        if (!possibleCancel(scheduleRes.concertDate(), scheduleRes.concertTime())) {
-//            throw new GlobalException(ErrorCase.CANNOT_CANCEL_BOOKING);
-//        }
-//
-//        paymentService.cancelPayment(getUserId, role, booking.getPayment().getId());
-//
-//        // Kafka에 예매 취소 메시지 전송
-//        String message = "Booking canceled for ID: " + bookingId;
-//        log.info("1. 예매 취소를 위한 Kafka 메시지 전송 준비 중입니다. Booking ID: {}", bookingId);
-//        kafkaProducer.sendMessage("booking-cancel-topic", message);
-//        log.info("4. 예매 취소를 위한 Kafka 메시지가 전송되었습니다. Booking ID: {}", bookingId);
-//    }
-
-    // ================= 테스트용 예매 취소 - 1 =================
     @Transactional
     public void cancelBooking(Long getUserId, String role, Long bookingId) {
 
@@ -309,20 +280,49 @@ public class BookingService {
         GetScheduleDetailRes scheduleRes = concertClient.getScheduleDetail(booking.getScheduleId());
 
         // 공연이 끝났는지 체크 필요
-//        if (!possibleCancel(scheduleRes.concertDate(), scheduleRes.concertTime())) {
-//            throw new GlobalException(ErrorCase.CANNOT_CANCEL_BOOKING);
-//        }
+        if (!possibleCancel(scheduleRes.concertDate(), scheduleRes.concertTime())) {
+            throw new GlobalException(ErrorCase.CANNOT_CANCEL_BOOKING);
+        }
 
-//        paymentService.cancelPayment(getUserId, role, booking.getPayment().getId());
+        paymentService.cancelPayment(getUserId, role, booking.getPayment().getId());
 
         // Kafka에 예매 취소 메시지 전송
         String message = "Booking canceled for ID: " + bookingId;
         log.info("1. 예매 취소를 위한 Kafka 메시지 전송 준비 중입니다. Booking ID: {}", bookingId);
-
         kafkaProducer.sendMessage("booking-cancel-topic", message);
-
         log.info("4. 예매 취소를 위한 Kafka 메시지가 전송되었습니다. Booking ID: {}", bookingId);
     }
+
+    // ================= 테스트용 예매 취소 - 1 =================
+//    @Transactional
+//    public void cancelBooking(Long getUserId, String role, Long bookingId) {
+//
+//        // TODO : 이거 좀 애매함 확인 필요 -> 결제 취소
+//        Booking booking = bookingRepository.findById(bookingId)
+//            .orElseThrow(() -> new GlobalException(ErrorCase.BOOKING_NOT_FOUND));
+//
+//        if (!role.equals("ROLE_MANAGER")) {
+//            if (!getUserId.equals(booking.getUserId())) {
+//                throw new GlobalException(ErrorCase.NOT_AUTHORIZED);
+//            }
+//        }
+//        GetScheduleDetailRes scheduleRes = concertClient.getScheduleDetail(booking.getScheduleId());
+//
+//        // 공연이 끝났는지 체크 필요
+////        if (!possibleCancel(scheduleRes.concertDate(), scheduleRes.concertTime())) {
+////            throw new GlobalException(ErrorCase.CANNOT_CANCEL_BOOKING);
+////        }
+//
+////        paymentService.cancelPayment(getUserId, role, booking.getPayment().getId());
+//
+//        // Kafka에 예매 취소 메시지 전송
+//        String message = "Booking canceled for ID: " + bookingId;
+//        log.info("1. 예매 취소를 위한 Kafka 메시지 전송 준비 중입니다. Booking ID: {}", bookingId);
+//
+//        kafkaProducer.sendMessage("booking-cancel-topic", message);
+//
+//        log.info("4. 예매 취소를 위한 Kafka 메시지가 전송되었습니다. Booking ID: {}", bookingId);
+//    }
 
     @Transactional
     public void deleteBooking(String email, Long bookingId) {
--- application/order-service/src/main/java/com/fortickets/orderservice/infrastructure/messaging/BookingCancelConsumer.java
+++ application/order-service/src/main/java/com/fortickets/orderservice/infrastructure/messaging/BookingCancelConsumer.java
@@ -1,13 +1,5 @@
 package com.fortickets.orderservice.infrastructure.messaging;
 
-import com.fortickets.common.exception.GlobalException;
-import com.fortickets.common.util.BookingStatus;
-import com.fortickets.common.util.ErrorCase;
-import com.fortickets.orderservice.domain.entity.Booking;
-import com.fortickets.orderservice.domain.repository.BookingRepository;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
@@ -16,55 +8,55 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class BookingCancelConsumer {
 
-//    @KafkaListener(topics = "booking-cancel-topic", groupId = "booking-group")
-//    public void consume(String message) {
-//        log.info("5. Kafka에서 메시지를 수신했습니다: {}", message);
-//
-//        // 메시지를 기반으로 비동기 작업 수행 추가(예: 알림 발송, 데이터베이스 상태 업데이트 등)
-//
-//    }
-//
-//    private void processBookingCancel(String message) {
-//        // 메시지 처리 로직 구현
-//        log.info("Processing booking cancel: {}", message);
-//    }
-
-    // ============== 테스트용 =================
-    private final BookingRepository bookingRepository;
-    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
-
-    public BookingCancelConsumer(BookingRepository bookingRepository) {
-        this.bookingRepository = bookingRepository;
-    }
-
     @KafkaListener(topics = "booking-cancel-topic", groupId = "booking-group")
     public void consume(String message) {
-        log.info("5. Kafka에서 메시지를 수신했습니다.: {}", message);
+        log.info("5. Kafka에서 메시지를 수신했습니다: {}", message);
 
-        // 10초 후에 상태 변경 작업 수행
-        log.info("6. 수신된 메시지를 기반으로 예약 취소 작업을 5초 후에 작동합니다. message: {}", message);
-        scheduler.schedule(() -> processBookingCancel(message), 5   , TimeUnit.SECONDS);
+        // 메시지를 기반으로 비동기 작업 수행 추가(예: 알림 발송, 데이터베이스 상태 업데이트 등)
+
     }
 
     private void processBookingCancel(String message) {
-        // 메시지에서 bookingId 추출
-        Long bookingId = extractBookingIdFromMessage(message);
-
-        // Booking 객체를 찾아 상태 변경
-        log.info("7. 예약 취소 작업을 처리 중입니다. Booking ID: {}", bookingId);
-        Booking booking = bookingRepository.findById(bookingId)
-            .orElseThrow(() -> new GlobalException(ErrorCase.BOOKING_NOT_FOUND));
-
-        booking.setStatus(BookingStatus.CANCELED);  // 상태 변경
-        bookingRepository.save(booking);
-
-        log.info("8. 예매 상태가 CANCELED로 변경되었습니다. Booking ID: {}", bookingId);
+        // 메시지 처리 로직 구현
+        log.info("Processing booking cancel: {}", message);
     }
 
-    private Long extractBookingIdFromMessage(String message) {
-        // 메시지에서 bookingId를 추출하는 로직 구현
-        // 예시: "Booking canceled for ID: 1" -> 1을 추출
-        return Long.parseLong(message.split(": ")[1]);
-    }
+    // ============== 테스트용 =================
+//    private final BookingRepository bookingRepository;
+//    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+//
+//    public BookingCancelConsumer(BookingRepository bookingRepository) {
+//        this.bookingRepository = bookingRepository;
+//    }
+//
+//    @KafkaListener(topics = "booking-cancel-topic", groupId = "booking-group")
+//    public void consume(String message) {
+//        log.info("5. Kafka에서 메시지를 수신했습니다.: {}", message);
+//
+//        // 10초 후에 상태 변경 작업 수행
+//        log.info("6. 수신된 메시지를 기반으로 예약 취소 작업을 5초 후에 작동합니다. message: {}", message);
+//        scheduler.schedule(() -> processBookingCancel(message), 5   , TimeUnit.SECONDS);
+//    }
+//
+//    private void processBookingCancel(String message) {
+//        // 메시지에서 bookingId 추출
+//        Long bookingId = extractBookingIdFromMessage(message);
+//
+//        // Booking 객체를 찾아 상태 변경
+//        log.info("7. 예약 취소 작업을 처리 중입니다. Booking ID: {}", bookingId);
+//        Booking booking = bookingRepository.findById(bookingId)
+//            .orElseThrow(() -> new GlobalException(ErrorCase.BOOKING_NOT_FOUND));
+//
+//        booking.setStatus(BookingStatus.CANCELED);  // 상태 변경
+//        bookingRepository.save(booking);
+//
+//        log.info("8. 예매 상태가 CANCELED로 변경되었습니다. Booking ID: {}", bookingId);
+//    }
+//
+//    private Long extractBookingIdFromMessage(String message) {
+//        // 메시지에서 bookingId를 추출하는 로직 구현
+//        // 예시: "Booking canceled for ID: 1" -> 1을 추출
+//        return Long.parseLong(message.split(": ")[1]);
+//    }
 
 }
