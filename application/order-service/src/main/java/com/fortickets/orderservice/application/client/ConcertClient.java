489405ec33c5ddbf17b5b90c018ae73ecc80effb
joo-chang <jooc0311@gmail.com>
2024-12-23 17:10:40 +0900

    fix(concert) :  feignclient patch 오류로 patch -> post 변경

--- application/order-service/src/main/java/com/fortickets/orderservice/application/client/ConcertClient.java
+++ application/order-service/src/main/java/com/fortickets/orderservice/application/client/ConcertClient.java
@@ -8,7 +8,6 @@ import java.util.List;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -40,7 +39,7 @@ public interface ConcertClient {
     @PostMapping("/concerts/list")
     List<GetConcertRes> getConcertsByIds(@RequestBody List<Long> concertIds);
 
-    @PatchMapping("/schedules/{scheduleId}/decrement")
+    @PostMapping("/schedules/{scheduleId}/decrement")
     GetScheduleDetailRes decrementSeats(@RequestBody DecrementScheduleReq decrementScheduleReq,
         @PathVariable(value = "scheduleId") Long scheduleId);
 }
